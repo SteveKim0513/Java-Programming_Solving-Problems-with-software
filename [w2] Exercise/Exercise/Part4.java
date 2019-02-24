eedc65afa929db54be9fcd8ee436a77f3e8534c7
SteveKim0513 <stevek4074@gmail.com>
2019-02-24 17:31:19 +0900

    no message

--- [w2] Exercise/Exercise/Part4.java
+++ [w2] Exercise/Exercise/Part4.java
@@ -5,11 +5,55 @@
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
+
+
+import edu.duke.URLResource;
+
+
 public class Part4 {
 
+    public void main() {
+        
+        URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        
+        System.out.println("==================================================");
+        
+        
+        for(String each_word : url.words()){
+            
+            String temp_word = each_word.toLowerCase();
+            int Index_youtube = temp_word.indexOf("youtube.com");
+
+            if(Index_youtube != -1){
+                int first_index = temp_word.indexOf("\"");
+                int last_index = temp_word.lastIndexOf("\"")+1;
+                
+                String result = each_word.substring(first_index, last_index);
+                System.out.println(result + "\n");
+            }
+            
+        }
+        
+        
+        
+        
+    
+    
+
+    }
     
     
     
+    
+    public void test(){
+    
+            URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+
+            for(String each_word : url.words()){
+                System.out.println(each_word);
+            }
+    
+    }
     
     
 }
