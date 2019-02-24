446123bb69fb269b7def93e4b62421ee0f95cff0
SteveKim0513 <stevek4074@gmail.com>
2019-02-24 14:38:26 +0900

    no message

--- [w2] Exercise/Exercise/Part3.java
+++ [w2] Exercise/Exercise/Part3.java
@@ -9,46 +9,85 @@ public class Part3 {
 
     public boolean twoOccurrences(String stringa, String stringb) {
     
-    
-    
+        String temp_str = stringb;
+        int temp_num = 0;
+        
+        int count = 0;
+        int length_a = stringa.length();
+        int length_b = stringb.length();
+               
+        for(int i=0 ; i < stringb.length(); ){
+            temp_num = temp_str.indexOf(stringa);
+            
+            if(temp_num == -1) break;
+            
+            else {
+                count ++;
+                temp_str = temp_str.substring(temp_num + length_a);
+                i = i + temp_num + length_a;
+            }
+        }
+        
+        if (count > 1) return true;
+        else return false;
     }
-    
+
     public String lastPart(String stringa, String stringb) {
-    
-    
-    
+        int index = stringb.indexOf(stringa);
+        int length_a = stringa.length();
+        
+        if (index == -1) return stringb;
+        
+        else return stringb.substring(index + length_a);    
     }
     
     
     
     public void test() {
     
+        System.out.println("============================================================");
+        
+        // True case
         String test1a = "an";
         String test1b = "banana";
         
-        boolean result1 = twoOccurrences(test1a, test1b);
-        System.out.println("Input= " + test1a + "," + test1b + "\n" + "Result= " +result1);
+        boolean result_twoOccurrences1 = twoOccurrences(test1a, test1b);
+        String result_lastPart1 = lastPart(test1a, test1b);
+        System.out.println("Input= " + test1a + "," + test1b + "\n" + "Result= " +result_twoOccurrences1);
+        System.out.println("Last Part= " + result_lastPart1);
         
+        System.out.println("\n");
         
-        String test2a = "atgaaabbbbbbttaatg";
-        String test2b = "atg";
+        // True case
+        String test2a = "atg";
+        String test2b = "atgaaabbbbbbttaatg";
         
-        boolean result2 = twoOccurrences(test2a, test2b);
-        System.out.println("Input= " + test2a + "," + test2b + "\n" + "Result= " +result2);
+        boolean result_twoOccurrences2 = twoOccurrences(test2a, test2b);
+        String result_lastPart2 = lastPart(test2a, test2b);
+        System.out.println("Input= " + test2a + "," + test2b + "\n" + "Result= " +result_twoOccurrences2);
+        System.out.println("Last Part= " + result_lastPart2);
         
+        System.out.println("\n");
         
+        // False case
         String test3a = "c";
         String test3b = "car";
         
-        boolean result3 = twoOccurrences(test3a, test3b);
-        System.out.println("Input= " + test3a + "," + test3b + "\n" + "Result= " +result3);
+        boolean result_twoOccurrences3 = twoOccurrences(test3a, test3b);
+        String result_lastPart3 = lastPart(test3a, test3b);
+        System.out.println("Input= " + test3a + "," + test3b + "\n" + "Result= " +result_twoOccurrences3);
+        System.out.println("Last Part= " + result_lastPart3);
         
+        System.out.println("\n");
         
+        // False case
         String test4a = "zoo";
         String test4b = "forest";
-        boolean result4 = twoOccurrences(test4a, test4b);
-        System.out.println("Input= " + test4a + "," + test4b + "\n" + "Result= " +result4);
         
+        boolean result_twoOccurrences4 = twoOccurrences(test4a, test4b);
+        String result_lastPart4 = lastPart(test4a, test4b);
+        System.out.println("Input= " + test4a + "," + test4b + "\n" + "Result= " +result_twoOccurrences4);
+        System.out.println("Last Part= " + result_lastPart4);
     }
     
     
