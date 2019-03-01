f25fad915ab2498088e161f85105be113ce4d29d
SteveKim0513 <stevek4074@gmail.com>
2019-03-01 12:12:22 +0900

    no message

--- [w1] perimeter_quiz/PerimeterAssignmentRunner.java
+++ [w1] perimeter_quiz/PerimeterAssignmentRunner.java
@@ -1,5 +1,32 @@
 import edu.duke.*;
 import java.io.File;
+//import aaa
+
+
+public double getPerimeter (Shape s) {
+        // Start with totalPerim = 0
+        double totalPerim = 0.0;
+        // Start wth prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim by currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }
+        // totalPerim is the answer
+        return totalPerim;
+    }
+    
+
+
+
+
+
+
 
 public class PerimeterAssignmentRunner {
     public double getPerimeter (Shape s) {
